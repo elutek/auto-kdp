ef461da87afa25f3bf69d6a8adaae4dfeabd9800
elutek <elalutek@gmail.com>
2022-11-16 22:19:03 -0800

    publish to check if the book is "ready"

--- src/action/publish.js
+++ src/action/publish.js
@@ -1,4 +1,4 @@
-import { Timeouts, Urls, debug } from './utils.js';
+import { Timeouts, Urls, debug, waitForElements } from './utils.js';
 
 export async function publish(book, params) {
   const verbose = params.verbose;
@@ -14,17 +14,42 @@ export async function publish(book, params) {
   await page.goto(url, { waitUntil: 'domcontentloaded', timeout: Timeouts.MIN_3 });
   await page.waitForTimeout(Timeouts.SEC_1);  // Just in case.
 
-  debug(verbose, 'Clicking publish');
-  await page.waitForSelector('#save-and-publish-announce', { timeout: Timeouts.MIN_1 });
-  await page.click('#save-and-publish-announce', { timeout: Timeouts.MIN_1 });
-  await page.waitForNavigation({ timeout: Timeouts.MIN_1 });
-  await page.waitForTimeout(Timeouts.SEC_1);  // Just in case.
+  await waitForElements(page, [
+    '#book-setup-navigation-bar-details-link .a-alert-content',
+    '#book-setup-navigation-bar-content-link .a-alert-content',
+    '#book-setup-navigation-bar-pricing-link .a-alert-content',
+  ]);
 
-  book.wasEverPublished = 'true';
+  debug(verbose, 'Checking if we can publish');
+
+  let id = '#book-setup-navigation-bar-details-link .a-alert-content';
+  const metadataStatus = await page.$eval(id, x => x.textContent.trim()) || '';
+
+  id = '#book-setup-navigation-bar-content-link .a-alert-content';
+  const contentStatus = await page.$eval(id, x => x.textContent.trim()) || '';
+
+  id = '#book-setup-navigation-bar-pricing-link .a-alert-content';
+  const pricingStatus = await page.$eval(id, x => x.textContent.trim()) || '';
+
+  let ok = (metadataStatus == 'Complete') && contentStatus == 'Complete' && pricingStatus == 'Complete';
+
+  let isSucess = ok;
+  if (ok) {
+    debug(verbose, 'Metadata, content and pricing status: OK');
+    debug(verbose, 'Clicking publish');
+    await page.waitForSelector('#save-and-publish-announce', { timeout: Timeouts.MIN_1 });
+    await page.click('#save-and-publish-announce', { timeout: Timeouts.MIN_1 });
+    await page.waitForNavigation({ timeout: Timeouts.MIN_1 });
+    await page.waitForTimeout(Timeouts.SEC_1);  // Just in case.
+
+    book.wasEverPublished = 'true';
+  } else {
+    debug(verbose, `Cannot publish! Metadata: ${metadataStatus}, content: ${contentStatus}, pricing: ${pricingStatus}`);
+  }
 
   if (!params.keepOpen) {
     await page.close();
   }
 
-  return true;
+  return isSucess;
 }
