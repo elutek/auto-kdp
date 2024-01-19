0ef2310c0c6532e52aeac97997b921294b638e66
Ela Krepska <e.krepska@gmail.com>
2024-01-18 16:54:22 -0800

    update

--- src/action/update-content.ts
+++ src/action/update-content.ts
@@ -149,9 +149,9 @@ export async function updateContent(book: Book, params: ActionParams): Promise<A
     {
         debug(book, verbose, 'Uploading cover');
         await page.waitForSelector('#data-print-book-publisher-cover-file-upload-browse-button-announce', Timeouts.SEC_3);
-        await page.selectFile('#data-print-book-publisher-cover-file-upload-browse-button-announce', book.coverLocalFile, Timeouts.MIN_3);
+        await page.selectFile('#data-print-book-publisher-cover-file-upload-browse-button-announce', book.coverLocalFile, Timeouts.MIN_20);
         debug(book, verbose, 'Waiting for cover file chooser');
-        await page.waitForSelectorVisible('#data-print-book-publisher-cover-file-upload-success', Timeouts.MIN_15);
+        await page.waitForSelectorVisible('#data-print-book-publisher-cover-file-upload-success', Timeouts.MIN_20);
         await page.waitForTimeout(Timeouts.SEC_2);
         debug(book, verbose, 'Cover upload done');
     }
@@ -160,9 +160,9 @@ export async function updateContent(book: Book, params: ActionParams): Promise<A
     {
         debug(book, verbose, 'Uploading manuscript');
         await page.waitForSelector('#data-print-book-publisher-interior-file-upload-browse-button-announce', Timeouts.SEC_3);
-        await page.selectFile('#data-print-book-publisher-interior-file-upload-browse-button-announce', book.manuscriptLocalFile, Timeouts.MIN_3);
+        await page.selectFile('#data-print-book-publisher-interior-file-upload-browse-button-announce', book.manuscriptLocalFile, Timeouts.MIN_20);
         debug(book, verbose, 'Waiting for manuscript file chooser');
-        await page.waitForSelectorVisible('#data-print-book-publisher-interior-file-upload-success', Timeouts.MIN_15);
+        await page.waitForSelectorVisible('#data-print-book-publisher-interior-file-upload-success', Timeouts.MIN_20);
     }
 
     // Click Launch previewer
